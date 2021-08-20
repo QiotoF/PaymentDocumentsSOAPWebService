88dfbee9cd93469a104f32ba0c73c2021ccd3ded
Дмитрий Марков <demarkov@miem.hse.ru>
2021-08-20 19:42:04 +0300

    Create WebServiceConfig.java

---
+++ server/src/main/java/com/qiotof/soapwebservice/WebServiceConfig.java
@@ -0,0 +1,22 @@
+package com.qiotof.soapwebservice;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(context);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "paymentDocuments")
+    public Wsdl11Definition defaultWsdl11Definition() {
+        SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
+        wsdl11Definition.setWsdl(new ClasPathResource("/resources/PaymentDocuments.wsdl"));
+        return wsdl11Definition;
+    }
+
+}

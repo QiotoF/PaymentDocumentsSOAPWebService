7176643532c0d13458d8c2990c4df1341fc7ef2c
Дмитрий Марков <demarkov@miem.hse.ru>
2021-08-22 14:28:58 +0300

    Create PaymentDocumentConfiguration

---
+++ client/src/main/java/com/qiotof/soapwebservice/PaymentDocumentConfiguration.java
@@ -0,0 +1,26 @@
+package com.qiotof.soapwebservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class PaymentDocumentConfiguration {
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        // this package must match the package in the <generatePackage> specified in
+        // pom.xml
+        marshaller.setContextPath("com.example.consumingwebservice.wsdl");
+        return marshaller;
+    }
+
+    @Bean
+    public PaymentDocumentClient paymentDocumentClient(Jaxb2Marshaller marshaller) {
+        PaymentDocumentClient client = new PaymentDocumentClient();
+        client.setDefaultUri("http://localhost:8080/ws");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        return client;
+    }
+}

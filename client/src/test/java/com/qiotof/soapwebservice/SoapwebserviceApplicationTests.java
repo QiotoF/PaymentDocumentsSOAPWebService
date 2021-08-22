9cdb62a26d64da90e9c100b152c8eebd97e4c6e6
Дмитрий Марков <demarkov@miem.hse.ru>
2021-08-22 14:03:47 +0300

    Init Spring for client

---
+++ client/src/test/java/com/qiotof/soapwebservice/SoapwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiotof.soapwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d2a9ec1c54611ebf24434151f446f543f5009a95
Дмитрий Марков <demarkov@miem.hse.ru>
2021-08-23 14:18:17 +0300

    Minor fixes

--- server/src/main/java/com/qiotof/soapwebservice/WebServiceConfig.java
+++ server/src/main/java/com/qiotof/soapwebservice/WebServiceConfig.java
@@ -10,7 +10,6 @@ import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.server.EndpointInterceptor;
 import org.springframework.ws.soap.server.endpoint.interceptor.PayloadValidatingInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
-import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;

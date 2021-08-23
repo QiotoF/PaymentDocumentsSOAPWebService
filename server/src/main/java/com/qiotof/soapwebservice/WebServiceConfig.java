7116ad73ec5bef33bee1438f9d5318d09147d1e8
Дмитрий Марков <demarkov@miem.hse.ru>
2021-08-23 11:50:47 +0300

    Add validating interceptors

--- server/src/main/java/com/qiotof/soapwebservice/WebServiceConfig.java
+++ server/src/main/java/com/qiotof/soapwebservice/WebServiceConfig.java
@@ -7,10 +7,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.server.endpoint.interceptor.PayloadValidatingInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+
+import java.util.List;
 
 @EnableWs
 @Configuration
@@ -22,6 +27,15 @@ public class WebServiceConfig extends WsConfigurerAdapter {
         servlet.setApplicationContext(context);
         servlet.setTransformWsdlLocations(true);
         return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        PayloadValidatingInterceptor validatingInterceptor = new PayloadValidatingInterceptor();
+        validatingInterceptor.setValidateRequest(true);
+        validatingInterceptor.setValidateResponse(true);
+        validatingInterceptor.setXsdSchema(new SimpleXsdSchema(new ClassPathResource("PaymentDocuments.xsd")));
+        interceptors.add(validatingInterceptor);
     }
 
     @Bean(name = "PaymentDocuments")

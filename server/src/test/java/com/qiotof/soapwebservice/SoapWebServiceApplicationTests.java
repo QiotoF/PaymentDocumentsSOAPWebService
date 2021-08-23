0c4d3192b897b9b3a9ada3b9027dc9349223c79f
Дмитрий Марков <demarkov@miem.hse.ru>
2021-08-23 16:51:27 +0300

    Add wsdl test

--- server/src/test/java/com/qiotof/soapwebservice/SoapWebServiceApplicationTests.java
+++ server/src/test/java/com/qiotof/soapwebservice/SoapWebServiceApplicationTests.java
@@ -1,13 +1,33 @@
 package com.qiotof.soapwebservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class SoapWebServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private WebTestClient webClient;
+
+	@Test
+	public void testWSDL() throws Exception {
+
+		webClient.get().uri("/ws/PaymentDocuments.wsdl")
+				.exchange().expectStatus().isOk();
+
 	}
 
 }
